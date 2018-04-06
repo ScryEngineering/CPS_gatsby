9e8c8a91315732a4ee50b2f036e5aa093ddcd8cb
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-06 21:38:56 +1000

    Update root index page

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,8 @@ import Link from 'gatsby-link'
 
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Welcome to Custom Programming Solutions</h1>
+    <p>TODO: content for front page</p>
   </div>
 )
 
