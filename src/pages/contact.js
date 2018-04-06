02ae0a32eea90d5bb9e2708b844f94ec727fcb83
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-06 21:41:41 +1000

    Created a contact page

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ContactPage = () => (
+  <div>
+    <h1>Contact us!</h1>
+    <p>TODO: content for contact page</p>
+  </div>
+)
+
+export default ContactPage
