d3d549ebfc4e837526f86d88a8e922428ec659f9
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-06 21:38:44 +1000

    Create about page

---
+++ src/pages/about.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>About CPS</h1>
+    <p>Custom programming solutions is a high end consulting firm that leverages technology to solve problems.
+    </p>
+    <p>
+    With many decades of collective experience in Business and technology we are well placed to deliver real gains on technology projects.
+    </p>
+    <p>
+    One of the biggest advantages we can bring as a result of our experience is knowing what our capabilites are.
+    In house we have experience with IT management, tech industry business strategy, project management, process improvement.
+    We also have in house technical skills in content management systems, data analysis, advanced Python development including internals and performance engineering, web development, data analysis, embedded systems.
+    We have a large network of professionals we partner with on projects, we regularly work with graphic designers and content writers along with other professionals that complement our core skills.
+    With this network we prefer to refer on work to others we know can deliver if we feel that we can't give a gaurantee we can do it.
+    </p>
+  </div>
+)
+
+export default IndexPage
