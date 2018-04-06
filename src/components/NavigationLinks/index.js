aec5e9adfb9e578b6eda5c4b5b82235df2e42712
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-06 21:22:58 +1000

    Create component for top navigation links

---
+++ src/components/NavigationLinks/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ListLink = props =>
+  <li style={{ display: `inline-block`, marginRight: `1rem` }}>
+    <Link to={props.to}>
+      {props.children}
+    </Link>
+  </li>
+
+const NavigationLinks = () => (
+  <div
+    style={{
+      background: 'green',
+      marginBottom: '1.45rem',
+    }}
+  >
+  <ul style={{ listStyle: `none`, float: `right` }}>
+    <ListLink to="/">Home</ListLink>
+    <ListLink to="/about/">About</ListLink>
+    <ListLink to="/contact/">Contact</ListLink>
+  </ul>
+  </div>
+)
+
+export default NavigationLinks
