c6dcfe5c1fdcc18fe36d190686d3cc5a0bb618e6
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-10 20:19:05 +1000

    Create tutorial pages from files

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,42 @@
  */
 
  // You can delete this file if you're not using it
+
+
+const path = require("path");
+
+exports.createPages = ({ boundActionCreators, graphql }) => {
+  const { createPage } = boundActionCreators;
+
+  const tutorialPostTemplate = path.resolve(`src/templates/tutorialTemplate.js`);
+
+  return graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `).then(result => {
+    if (result.errors) {
+      return Promise.reject(result.errors);
+    }
+
+    result.data.allMarkdownRemark.edges.forEach(({ node }) => {
+      createPage({
+        path: path.join("tutorials", node.frontmatter.path),
+        component: tutorialPostTemplate,
+        context: {}, // additional data can be passed via context
+      });
+    });
+  });
+};
+
