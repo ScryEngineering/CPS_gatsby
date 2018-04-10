2670ee86d3949e9c2b7cc4a38de32182cc7d1693
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-04-10 19:09:52 +1000

    List tutorial pages in tutorials tab

--- src/pages/tutorials.js
+++ src/pages/tutorials.js
@@ -1,10 +1,40 @@
 import React from 'react'
 
-const TutorialsPage = () => (
-  <div>
-    <h1>Tutorials</h1>
-    <p>Tutorials coming soon</p>
-  </div>
-)
+export default ({ data }) => {
+  console.log(data);
+  return (
+    <div>
+      <h1 display={"inline-block"} borderBottom={"1px solid"}>
+        Tutorial pages
+      </h1>
+      <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <div key={node.id}>
+          <h3>
+            {node.frontmatter.title}{" "}
+            <span color="#BBB">— {node.frontmatter.date}</span>
+          </h3>
+          <p>{node.excerpt}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default TutorialsPage
+export const query = graphql`
+  query IndexQuery {
+    allMarkdownRemark {
+      totalCount
+      edges {
+        node {
+          id
+          frontmatter {
+            title
+            date(formatString: "DD MMMM, YYYY")
+          }
+          excerpt
+        }
+      }
+    }
+  }
+`;
