74eb220fd71ef0c02914bd85fb49467253b17b8e
Janis <janis@jaggedverge.com>
2018-04-15 02:55:43 +1000

    Stub template for personal about page creation

---
+++ src/templates/person.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default class PersonalAboutTemplate extends React.Component {
+    render(){
+
+      return (
+        <div>
+          <h1>
+            Page for person
+          </h1>
+        </div>
+      );
+    }
+  }
