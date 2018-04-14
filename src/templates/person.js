a8c2c8a3e31450e57328dca7daf78090b615bee9
Janis <janis@jaggedverge.com>
2018-04-15 03:49:45 +1000

    Create template for personal about page

--- src/templates/person.js
+++ src/templates/person.js
@@ -2,13 +2,33 @@ import React from "react";
 
 export default class PersonalAboutTemplate extends React.Component {
     render(){
+      //const name = this.props.data.name;
 
       return (
         <div>
           <h1>
-            Page for person
+            About (individual)
           </h1>
         </div>
       );
     }
+};
+
+  /* eslint no-undef: "off" */
+export const pageQuery = graphql`
+query AuthorPage($author: String) {
+  allPeopleJson(filter: { id: { eq: $author } }) {
+    edges {
+      node {
+        id
+        name
+        image
+        url
+        bio
+        location
+        socialUrls
+      }
+    }
   }
+}
+`;
