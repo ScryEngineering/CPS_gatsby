80f1918359bd438dcdb0f5a5438a0b079d033b5c
Janis <janis@jaggedverge.com>
2018-04-14 23:44:26 +1000

    Implement Footer component

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,32 @@
+import React, { Component } from "react";
+import styles from "./Footer.module.css";
+
+class Footer extends Component {
+  render() {
+    const PoweredBy = props => {
+      const { show } = props;
+      if (show) {
+        return (
+          <section className={styles.poweredby}>
+            Proudly published with <a href="https://gatsbyjs.org">Gatsby</a>
+          </section>
+        );
+      }
+      return null;
+    };
+
+    const { promoteGatsby } = this.props;
+    const { label, url, year } = this.props.copyright;
+    return (
+      <footer className={styles.siteFooter} clearfix>
+        <section className={styles.copyright}>
+          <a href={url || "/"}>{label}</a> &copy;{" "}
+          {year || new Date().getFullYear()}
+        </section>
+        <PoweredBy show={promoteGatsby} />
+      </footer>
+    );
+  }
+}
+
+export default Footer;
