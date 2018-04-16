5605e2ec83773e45091c284451f070cc959e45b8
Janis <janis@jaggedverge.com>
2018-04-16 22:29:10 +1000

    Make author component take name and link params

--- src/components/Author/Author.js
+++ src/components/Author/Author.js
@@ -1,10 +1,28 @@
 import React from "react";
+import Link from 'gatsby-link'
+
+
+import PropTypes from 'prop-types';
 
 export default class Author extends React.Component{
   render(){
     console.log("in Author component", this.props);
+    const {name, authorLink} = this.props;
+    let authorNameElement = ""
+    if (authorLink === undefined){
+      authorNameElement = <p>{name}</p>;
+    }else{
+      authorNameElement = <Link to={authorLink}>{name}</Link>;
+    }
+
     return(
-      <div>{this.props.authorName}</div>
+      <div>
+        {authorNameElement}
+      </div>
     );
   }
+}
+
+Author.propTypes = {
+  name: React.PropTypes.string.isRequired
 }
