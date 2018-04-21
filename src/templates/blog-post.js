189d9ad988e1346109998d2b6856553bc1b02b4a
Janis <janis@jaggedverge.com>
2018-04-21 19:25:27 +1000

    Create template for blog post creation

---
+++ src/templates/blog-post.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Helmet from 'react-helmet'
+
+import PostTags from "../components/PostTags/PostTags";
+
+export default function Template({
+  data, // this prop will be injected by the GraphQL query below.
+}) {
+  console.log(data)
+  const post = data.markdownRemark; // data.markdownRemark holds our post data
+  const postHasTags = post.frontmatter.tags !== null && post.frontmatter.tags.length > 0
+  return (
+    <div className="blog-post-container">
+      <Helmet>
+        <title>{post.frontmatter.title}</title>
+      </Helmet>
+      <div className="blog-post">
+        <h1>{post.frontmatter.title}</h1>
+        <h2>{post.frontmatter.author} {post.frontmatter.date}</h2>
+        <div
+          className="blog-post-content"
+          dangerouslySetInnerHTML={{ __html: post.html }}
+        />
+        { postHasTags &&
+          <PostTags tags={post.frontmatter.tags} />
+        }
+      </div>
+    </div>
+  );
+}
+
+export const query = graphql`
+  query BlogPostQuery($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        date(formatString: "DD MMMM, YYYY")
+        title
+        tags
+        author
+        draft
+      }
+    }
+  }
+`;
