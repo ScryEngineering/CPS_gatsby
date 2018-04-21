dbbe59c80adcfdefdeb680ec9f114040399b765f
Janis <janis@jaggedverge.com>
2018-04-21 19:23:45 +1000

    Create blog landing page

---
+++ src/pages/blog.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+import Link from "gatsby-link";
+
+import styles from "./tutorials.module.css"
+import PostListing from "../components/PostListing/PostListing";
+
+export default class BlogListingTemplate extends React.Component {
+  render(){
+    const postEdges = this.props.data.allMarkdownRemark.edges;
+    const numberOfPosts = this.props.data.allMarkdownRemark.totalCount;
+    const allAuthors = this.props.data.authors.edges;
+    return (
+      <div>
+        <Helmet>
+          <title>Blog</title>
+        </Helmet>
+        <h1>
+          Blog posts
+        </h1>
+        <h4>{numberOfPosts} Posts</h4>
+        <PostListing postEdges={postEdges} allAuthorsInfo={allAuthors}/>
+      </div>
+    );
+  }
+}
+
+export const query = graphql`
+query IndexQuery {
+  # authors
+  authors: allPeopleJson {
+    edges {
+      node {
+        id
+        name
+        image
+        url
+        bio
+        location
+        socialUrls
+        fields {
+          internalURL
+        }
+      }
+    }
+  }
+  # tutorial posts
+  allMarkdownRemark(
+    sort: { fields: [frontmatter___date], order: DESC }
+    filter: { frontmatter: { draft: { ne: true } } }
+  ) {
+    totalCount
+    edges {
+      node {
+        fields {
+          slug
+        }
+        excerpt
+        frontmatter {
+          title
+          tags
+          date(formatString: "DD MMMM, YYYY")
+          author
+          draft
+        }
+      }
+    }
+  }
+}
+`;
