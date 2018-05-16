08cec2706c996f34b04a345292feabb744afe2bb
Janis <janis@jaggedverge.com>
2018-05-17 03:45:13 +1000

    Fix typo

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -11,7 +11,7 @@ const ContactPage = () => (
     <h1>Contact us!</h1>
     <p>
       Do you have a question about projects, technologies, tutorial posts you'd like to see or something else that is of interest?
-      Looking to find techincal talent in the Melbourne, Australia or Toronto, Canada areas?
+      Looking to find technical talent in the Melbourne, Australia or Toronto, Canada areas?
       If you would like to start a discussion about how we can help you we'd love to hear from you, send us an email: hello at customprogrammingsolutions.com
     </p>
   </div>
