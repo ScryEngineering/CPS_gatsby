307496bc84f95144d325dadc1b6bcbd1c4a4c8df
Janis <janis@jaggedverge.com>
2018-05-22 03:43:03 +1000

    Switch theme

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,6 +1,7 @@
 import Typography from "typography";
-import lawtonTheme from "typography-theme-lawton";
+//import lawtonTheme from "typography-theme-lawton";
+import noriegaTheme from 'typography-theme-noriega'
 
-const typography = new Typography(lawtonTheme);
+const typography = new Typography(noriegaTheme)
 
 export default typography;
