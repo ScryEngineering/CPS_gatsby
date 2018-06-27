bf50d94951a3d28731a262e9c1172f6f6a96046a
Janis <janis@jaggedverge.com>
2018-06-27 16:07:04 +1000

    Use logo for header on site

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -4,14 +4,14 @@ import Link from 'gatsby-link'
 const Header = () => (
   <div
     style={{
-      background: 'rebeccapurple',
+      background: 'black',
     }}
   >
     <div
       style={{
         margin: '0 auto',
         maxWidth: 960,
-        padding: '1.45rem 1.0875rem',
+        padding: '0.45rem 1.0875rem',
       }}
     >
       <h1 style={{ margin: 0 }}>
@@ -22,7 +22,7 @@ const Header = () => (
             textDecoration: 'none',
           }}
         >
-          Custom Programming Solutions
+          <img src="/CPS-BW-header.png" alt="Custom Programming Solutions"></img>
         </Link>
       </h1>
     </div>
