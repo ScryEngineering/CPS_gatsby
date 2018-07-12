6f1bf6ea118dfd9d77a760e9df8e4bcd2a0dbce2
Aapeli <git@aapelivuorinen.com>
2018-07-12 18:33:03 +1000

    Tweak padding on content pages

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -23,14 +23,7 @@ const TemplateWrapper = ({ children, data }) => (
     />
     <Header />
     <NavigationLinks />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
+    <div className="contentdiv">
       {children()}
     </div>
     <Footer
