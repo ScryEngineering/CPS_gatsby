d062ca013b17596fd722ecbd5a68e5bfac295958
Aapeli <git@aapelivuorinen.com>
2018-07-15 00:38:17 +1000

    Unify spacing of header, content, and footer, and clean up footer. Make footer stay at bottom of page even on smaller pages

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,17 +6,19 @@ class Footer extends Component {
   render() {
     const { label, url, year } = this.props.copyright;
     return (
-      <footer className={styles.siteFooter}>
-        <section className={styles.logo}>
-          <img src="/logo-CPS.png" alt="CPS logo"></img>
-        </section>
-        <section className={styles.copyright}>
-          <a href={url || "/"} className={styles.cpsLink}>{label}</a> &copy;{" "}
-          {year || new Date().getFullYear()}
-        </section>
-        <section className={styles.socialButtons}>
-          <SocialIcon url="https://github.com/customprogrammingsolutions" />
-        </section>
+      <footer className={styles.wrapper}>
+        <div className="footer">
+          <section className={styles.logo}>
+            <img src="/logo-CPS.png" alt="CPS logo"></img>
+          </section>
+          <section className={styles.copyright}>
+            <a href={url || "/"} className={styles.cpsLink}>{label}</a> &copy;{" "}
+            {year || new Date().getFullYear()}
+          </section>
+          <section className={styles.socialButtons}>
+            <SocialIcon url="https://github.com/customprogrammingsolutions" />
+          </section>
+        </div>
       </footer>
     );
   }
