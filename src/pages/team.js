6745cefe9258a06f635e72fd6b1367a79800b25c
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-07-15 00:41:46 +1000

    Stub team page

---
+++ src/pages/team.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Helmet from 'react-helmet'
+
+export default class TeamPage extends React.Component {
+  render(){
+    return (
+        <div>
+          <Helmet>
+            <title>The CPS team</title>
+          </Helmet>
+          <h1>
+            Coming soon
+          </h1>
+        </div>
+      );
+  }
+}
