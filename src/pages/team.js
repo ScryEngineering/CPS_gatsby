f325ed1f04247716f7f3dd4c6acaff0cf1de3817
Janis Lesinskis <janis@customprogrammingsolutions.com>
2018-07-15 01:02:29 +1000

    GraphQL query to extract team members

--- src/pages/team.js
+++ src/pages/team.js
@@ -3,15 +3,42 @@ import Helmet from 'react-helmet'
 
 export default class TeamPage extends React.Component {
   render(){
+    const allTeamMembers = this.props.data.teamMembers.edges;
     return (
-        <div>
-          <Helmet>
-            <title>The CPS team</title>
-          </Helmet>
-          <h1>
-            Coming soon
-          </h1>
-        </div>
-      );
+      <div>
+        <Helmet>
+          <title>The CPS team</title>
+        </Helmet>
+        <h1>
+        Coming soon
+        </h1>
+        <ul>
+        {allTeamMembers.map(person => (
+            <li>{person.node.name}</li>
+        ))}
+        </ul>
+      </div>
+    );
   }
 }
+
+export const query = graphql`
+query TeamQuery {
+  # authors
+  teamMembers: allPeopleJson {
+    edges {
+      node {
+        id
+        name
+        image
+        url
+        bio
+        location
+        socialUrls
+        fields {
+          internalURL
+        }
+      }
+    }
+  }
+}`
