9cffc16081188bebca5018596f4d4091921d0951
Aapeli <git@aapelivuorinen.com>
2018-07-15 11:54:31 +1000

    Possibly nice layout for team page

--- src/pages/team.js
+++ src/pages/team.js
@@ -2,24 +2,37 @@ import React from 'react'
 import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
 
+import styles from './team.module.css'
+
+const TeamMemberSection = props =>
+  <section className={styles.teamMemberSection}>
+    <div className={styles.teamMemberPhotoContainer}>
+      <img src="http://via.placeholder.com/400x400" className={styles.teamMemberPhoto} />
+    </div>
+    <div className={styles.teamMemberDetailsContainer}>
+      <h2>{props.name}</h2>
+      <p>{props.name}</p>
+    </div>
+  </section>
 
 export default class TeamPage extends React.Component {
   render(){
+    console.log(this.props)
     const allTeamMembers = this.props.data.teamMembers.edges;
     console.log("allTeamMembers: ", allTeamMembers);
     return (
       <div>
         <Helmet>
-          <title>The CPS team</title>
+          <title>Our Team</title>
         </Helmet>
-        <h1>
-          The CPS team
-        </h1>
+        <h1>Our Team</h1>
+        <p>Something short about how awesome we are</p>
         <ul>
         {allTeamMembers.map(person => (
-            <li><Link to={person.node.fields.internalURL}>{person.node.name}</Link></li>
+          <TeamMemberSection name={person.node.name} />
         ))}
         </ul>
+        {/*<ContactSnippet blurb="Interested in how our expert team could transform your business? Fill out the form below and one of them will get in touch with you soon." />*/}
       </div>
     );
   }
