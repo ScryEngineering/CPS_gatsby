923a49cea80980241aaca297a75b184755a09c99
Aapeli <git@aapelivuorinen.com>
2018-07-29 19:46:59 +1000

    Add gatsby-plugin-sitemap to generate a sitemap

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,6 +2,7 @@
 module.exports = {
   peopleDir: "people", // The name of directory that contains your 'people' folder.
   defaultAuthorName: "Custom Programming Solutions", // The default and fallback author ID used for blog posts without a defined author.
+  siteUrl: "https://www.customprogrammingsolutions.com",
   contentDir: process.env.CONTENT_DIR || "content/",
   contactFormEndpoint: process.env.GATSBY_CONTACT_FORM_ENDPOINT || "http://localhost/contact-form-api"
 };
