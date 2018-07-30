39d890a4f48d8b6fdf9c9ed10518aee3ca43c676
Aapeli <git@aapelivuorinen.com>
2018-07-30 12:43:22 +1000

    Tweak colours

--- src/components/Masthead/Masthead.js
+++ src/components/Masthead/Masthead.js
@@ -7,8 +7,8 @@ const Header = () => (
   <div className={styles.wrapper}>
     <div className={"masthead "+styles.masthead}>
       <span>
-        <h1>Welcome.</h1>
-        <p>Lorem ipsum dolor sit amet</p>
+        <h1>High-tech software consulting in Melbourne.</h1>
+        <p>We are a versatile programming, software development and consulting firm driven by trusted industry experts. Our clients are agile organizations who require high-quality, custom built solutions.</p>
       </span>
     </div>
   </div>
