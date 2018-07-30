eb0e37480be526e45b90cb4d29e521a55f34cc20
Aapeli <git@aapelivuorinen.com>
2018-07-30 12:54:29 +1000

    Remove typography.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,12 +8,6 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
-      options: {
-        pathToConfigModule: `src/utils/typography.js`,
-      },
-    },
-    {
       resolve: `gatsby-source-filesystem`,
       options: {
         name: `tutorials`,
