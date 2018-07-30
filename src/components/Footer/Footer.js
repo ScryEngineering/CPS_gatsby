76f712fbe3adea57ed2a0b05254125b3e6a28abe
Aapeli <git@aapelivuorinen.com>
2018-07-30 14:45:17 +1000

    Work on footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,23 +8,40 @@ class Footer extends Component {
     return (
       <footer className={styles.wrapper}>
         <div className={styles.footer}>
-          <div className={styles.top}>
+          <div className={styles.content}>
+            <div className={styles.left}>
+              <div className={styles.group}>
+                <h4>Get in touch</h4>
+                <ul>
+                  <li>Contact form</li>
+                  <li>LinkedIn</li>
+                  <li>Facebook</li>
+                  <li>Twitter</li>
+                  <li>RSS</li>
+                  <li>GitHub</li>
+                  <li>Email</li>
+                </ul>
+              </div>
+              <div className={styles.group}>
+                <h4>Pages</h4>
+                <ul>
+                  <li>Research</li>
+                  <li>Case studies</li>
+                  <li>Blogs &amp; Tutorials</li>
+                  <li>Team</li>
+                </ul>
+              </div>
+            </div>
+            <div className={styles.copyright}>
+              &copy;{" "}{year || new Date().getFullYear()}{" "}
+              <a href={url || "/"} className={styles.cpsLink}>{label}</a><br />
+              ABN: 34627525197<br />
+              ACN: 627 525 197
+            </div>
           </div>
-          <section className={styles.logoContainer}>
-            <img src="/logo-CPS.png" alt="CPS logo"></img>
-          </section>
-          <section className={styles.copyright}>
-            Copyright{" "}{year || new Date().getFullYear()}<br />
-            <a href={url || "/"} className={styles.cpsLink}>{label}</a><br />
-            ACN: 627 525 197<br />
-            ABN: 34627525197
-          </section>
-          <section className={styles.socialButtons}>
-            <SocialIcon url="https://github.com/customprogrammingsolutions" />
-            <SocialIcon url="https://twitter.com/C_P_S_online" />
-            <SocialIcon url="https://www.linkedin.com/company/18716004/" />
-            <SocialIcon url="/rss.xml" network="rss" />
-          </section>
+          <div className={styles.bottom}>
+            <img height="50" src="/logo-CPS.png" alt="CPS logo"></img>
+          </div>
         </div>
       </footer>
     );
