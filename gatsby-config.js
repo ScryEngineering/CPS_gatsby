e7a1a2d521b38fe2fa2f55c9b3d95e3447419e10
Janis <janis@customprogrammingsolutions.com>
2018-08-01 22:25:34 +1000

    Create rss feed via query that only takes in posts

--- gatsby-config.js
+++ gatsby-config.js
@@ -82,7 +82,41 @@ module.exports = {
       resolve: `gatsby-plugin-sitemap`
     },
     {
-      resolve: `gatsby-plugin-feed`
+      resolve: `gatsby-plugin-feed`,
+      options: {
+        feeds: [
+          {
+            query: `
+            {
+              allMarkdownRemark(
+                limit: 1000,
+                sort: {
+                  order: DESC,
+                  fields: [frontmatter___date]
+                },
+                filter: {
+                  fields: { isPost: { eq: true }}
+                }
+              ) {
+                edges {
+                  node {
+                    frontmatter {
+                      title
+                      date
+                    }
+                    fields {
+                      slug
+                    }
+                    excerpt
+                    html
+                  }
+                }
+              }
+            }`,
+            output: `/rss.xml`
+          }
+        ]
+      }
     }
   ],
 };
