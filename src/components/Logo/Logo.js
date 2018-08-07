20d21beeeee878e0bb9346294753246af0d2f903
Aapeli <git@aapelivuorinen.com>
2018-08-07 18:23:44 +1000

    Fix react bug

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import styles from "./Logo.module.scss";
 export default class Logo extends React.Component {
   render(){
     return (
-      <div className={styles.logoContainer} style={{"height": this.props.height, "font-size": this.props.height}}>
+      <div className={styles.logoContainer} style={{"height": this.props.height, "fontSize": this.props.height}}>
         <Link to="/" className={styles.logo + " " + (this.props.dark ? styles.logodark : styles.logomain)}>
           <img src={this.props.dark ? "/CPS-logo-dark.svg" : "/CPS-logo-main.svg"} alt="Logo" style={{"height": this.props.height * 0.9}}></img>
           <span>Custom Programming Solutions</span>
