2fa1b1e965fc805c0940a85eda2733bd04a67d0e
Aapeli <git@aapelivuorinen.com>
2018-08-07 22:06:35 +1000

    Upgrade to new layout

--- src/pages/team.js
+++ src/pages/team.js
@@ -1,6 +1,10 @@
 import React from 'react'
-import Helmet from 'react-helmet'
 import Link from 'gatsby-link'
+
+import HelmetWrapper from "../components/HelmetWrapper/HelmetWrapper";
+
+import Masthead from '../components/Masthead/Masthead'
+import ContactSnippet from "../components/ContactSnippet/ContactSnippet";
 
 import styles from './team.module.css'
 
@@ -10,9 +14,9 @@ const TeamMemberSection = props =>
       <img src="http://via.placeholder.com/400x400" className={styles.teamMemberPhoto} />
     </div>
     <div className={styles.teamMemberDetailsContainer}>
-      <h2 className={styles.teamMemberName}>{props.person.name}</h2>
-      <p className={styles.teamMemberTitle}>{props.person.bio}, <span className={styles.teamMemberLocation}>{props.person.location}</span></p>
-      <p>{props.person.bio}... <Link to={props.person.fields.internalURL}>Read more.</Link></p>
+      <h2 className={styles.teamMemberName}>{props.person.frontmatter.name}</h2>
+      <p className={styles.teamMemberTitle}>{props.person.frontmatter.bio}, <span className={styles.teamMemberLocation}>{props.person.frontmatter.location}</span></p>
+      <p>{props.person.frontmatter.bio}... <Link to={props.person.fields.internalURL}>Read more.</Link></p>
     </div>
   </section>
 
@@ -23,17 +27,14 @@ export default class TeamPage extends React.Component {
     console.log("allTeamMembers: ", allTeamMembers);
     return (
       <div>
-        <Helmet>
-          <title>Our Team</title>
-        </Helmet>
-        <h1>Our Team</h1>
-        <p>Something short about how awesome we are</p>
-        <div className={styles.teamMembersContainer}>
-        {allTeamMembers.map(person => (
-          <TeamMemberSection person={person.node} />
-        ))}
+        <HelmetWrapper title="Our Team" />
+        <Masthead heading="Our Team" />
+        <div className="contentdiv">
+          {allTeamMembers.map(person => (
+            <TeamMemberSection person={person.node} />
+          ))}
+          <ContactSnippet blurb="Interested in how our expert team could transform your business? Fill out the form below and one of them will get in touch with you soon." />
         </div>
-        {/*<ContactSnippet blurb="Interested in how our expert team could transform your business? Fill out the form below and one of them will get in touch with you soon." />*/}
       </div>
     );
   }
