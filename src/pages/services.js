d933fcd490e5c9da2fc7869595f79b2cdd284766
Aapeli <git@aapelivuorinen.com>
2018-09-07 18:24:59 +1000

    Clean up HTML on services page

--- src/pages/services.js
+++ src/pages/services.js
@@ -9,8 +9,7 @@ const ServicesPage = props => (
     <HelmetWrapper title="Services" />
     <Masthead heading="Our services" />
     <div className="contentdiv">
-    <p>
-      With a collective industry experience of multiple decades we are well placed to offer our consulting services in the following areas:
+      <p>With a collective industry experience of multiple decades we are well placed to offer our consulting services in the following areas:</p>
       <ul>
         <li>Business strategy</li>
         <li>Technology management</li>
@@ -21,20 +20,16 @@ const ServicesPage = props => (
         <li>Machine learning/AI systems</li>
         <li>Embedded systems and Linux kernel development</li>
       </ul>
-      We are particularly keen to work on projects where we can partner to deliver value via our services.
-
-      We also take a very strong interest in education and regularly attend and present at various workshops and conferences.
-      We can help upskill your team or work as coaches to intermediate to senior talent looking to take their skills up to the next level.
-
+      <p>We are particularly keen to work on projects where we can partner to deliver value via our services.</p>
+      <p>We also take a very strong interest in education and regularly attend and present at various workshops and conferences.</p>
+      <p>We can help upskill your team or work as coaches to intermediate to senior talent looking to take their skills up to the next level.</p>
       <ul>
         <li>Educational strategy consulting, including bringing our expertise in educational settings to help your organization be more efficient with training and professional development.</li>
         <li>Provide in house training programs and workshops on specialized topics</li>
       </ul>
-      If you are looking for mentors/training for community events please feel free to contact us, we are strong proponents of the technical community in general and are always looking for opportunities to help people help the community.
-
-    </p>
-    <ContactSnippet source={props.location.pathname} blurb="Would you like to discuss how we could help you? Fill out the form below and we'll get in touch as soon as possible." />
-  </div>
+      <p>If you are looking for mentors/training for community events please feel free to contact us, we are strong proponents of the technical community in general and are always looking for opportunities to help people help the community.</p>
+      <ContactSnippet source={props.location.pathname} blurb="Would you like to discuss how we could help you? Fill out the form below and we'll get in touch as soon as possible." />
+    </div>
   </div>
 )
 
