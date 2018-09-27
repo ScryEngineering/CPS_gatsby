e8a479ea907b9937cce4b303341a728321a4b682
Aapeli <git@aapelivuorinen.com>
2018-09-27 18:09:44 +1000

    Use new PostListing.js for tag pages

--- src/templates/tag.js
+++ src/templates/tag.js
@@ -8,18 +8,12 @@ import PostListing from "../components/PostListing/PostListing";
 export default class TagTemplate extends React.Component {
   render(){
     const tag = this.props.pathContext.tag;
-    const postEdges = this.props.data.allMarkdownRemark.edges;
-    const numberOfPosts = this.props.data.allMarkdownRemark.totalCount;
-    const allAuthors = this.props.data.authors.edges;
-    const filteredPosts = postEdges.filter(postEdges => postEdges.node.frontmatter.draft !== true);
-    var postCount = filteredPosts.filter(() => true).length;
     return (
       <div>
         <HelmetWrapper title={tag + " posts"} />
         <Masthead heading={"Posts tagged with \"" + tag + "\""} />
         <div className="contentdiv">
-          <h4>{postCount} posts</h4>
-          <PostListing postEdges={filteredPosts} allAuthorsInfo={allAuthors}/>
+          <PostListing postEdges={this.props.data.allMarkdownRemark.edges} allAuthorsInfo={this.props.data.authors.edges} />
         </div>
       </div>
     );
