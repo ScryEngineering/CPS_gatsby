04d77a90fa12f92aafda62a3bb63fce820d4ecb5
Janis Lesinskis <shuttle1987@users.noreply.github.com>
2018-11-16 11:07:48 +1100

    Remove services we are no longer offering

--- src/pages/services.js
+++ src/pages/services.js
@@ -17,17 +17,9 @@ const ServicesPage = props => (
         <li>Machine learning and artificial intelligence</li>
         <li>Process automation and optimization</li>
         <li>Cryptography and security</li>
-        <li>Embedded systems and Linux kernel development</li>
         <li>Mathematical optimization</li>
       </ul>
       <p>We are particularly keen to work on projects where we can partner to deliver value via our services.</p>
-      <p>We also take a very strong interest in education and regularly attend and present at various workshops and conferences.</p>
-      <p>We can help upskill your team or work as coaches to intermediate to senior talent looking to take their skills up to the next level.</p>
-      <ul>
-        <li>Educational strategy consulting, including bringing our expertise in educational settings to help your organization be more efficient with training and professional development.</li>
-        <li>Provide in house training programs and workshops on specialized topics</li>
-      </ul>
-      <p>If you are looking for mentors/training for community events please feel free to contact us, we are strong proponents of the technical community in general and are always looking for opportunities to help people help the community.</p>
       <ContactSnippet source={props.location.pathname} blurb="Would you like to discuss how we could help you? Fill out the form below and we'll get in touch as soon as possible." />
     </div>
   </div>
