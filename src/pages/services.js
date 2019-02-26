12993d65f9e7ba089f27e9e81353e0e44e122315
Janis Lesinskis <janis@customprogrammingsolutions.com>
2019-02-26 18:26:41 +1100

    Added link to the process automation service page

--- src/pages/services.js
+++ src/pages/services.js
@@ -15,10 +15,10 @@ const ServicesPage = props => (
         <li><a href="/services/data-science">Data science</a></li>
         <li>Blockchain and distributed systems</li>
         <li>Business strategy and technology management</li>
-        <li>Cloud services and DevOps</li>
         <li>Machine learning and artificial intelligence</li>
-        <li>Process automation and optimization</li>
+        <li><a href="/services/process-automation">Process automation and optimization</a></li>
         <li>Cryptography and security</li>
+        <li>Cloud services and DevOps</li>
       </ul>
       <p>We are particularly keen to work on projects where we can partner to deliver value via our services.</p>
       <ContactSnippet source={props.location.pathname} blurb="Would you like to discuss how we could help you? Fill out the form below and we'll get in touch as soon as possible." />
