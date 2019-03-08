ddec0d79a5a7f7e21195c566597f82d0f7031f41
Janis <janis@customprogrammingsolutions.com>
2019-03-08 22:00:08 +1100

    Added check for URL before rendering it

--- src/templates/person.js
+++ src/templates/person.js
@@ -12,13 +12,16 @@ export default class PersonalAboutTemplate extends React.Component {
       const data = this.props.data.authors.edges[0].node;
       const postEdges = this.props.data.allMarkdownRemark.edges;
       const allAuthors = this.props.data.allAuthors.edges;
+      const author_has_personal_URL = data.frontmatter.url !== null && data.frontmatter.url !== undefined;
       return (
         <div>
           <HelmetWrapper title={data.frontmatter.name} description={data.frontmatter.miniBlurb} />
           <Masthead heading={"About " + data.frontmatter.name} paragraph={data.frontmatter.bio + ", " + data.frontmatter.location} />
           <div className="contentdiv">
             <div className="post-content" dangerouslySetInnerHTML={{ __html: data.html }} />
+            { author_has_personal_URL &&
             <div className="main-url">{data.frontmatter.name} has a personal website here: <a href="{data.frontmatter.url}">{data.frontmatter.url}</a> </div>
+            }
             <h2 className={styles.postsby}>Posts by {data.frontmatter.name}</h2>
             <PostListing postEdges={postEdges} allAuthorsInfo={allAuthors} filter={post => post.node.frontmatter.authors && post.node.frontmatter.authors.indexOf(data.frontmatter.name) >= 0}/>
           </div>
